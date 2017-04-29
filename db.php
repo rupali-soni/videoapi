2d028e8a502355844ff3c55461da6398485ad09d
Rupali Soni <rupali.soni19@gmail.com>
2017-04-29 19:21:32 +0530

    db update

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 function getDB() {
     
-	$dbhost="localhost";
-    $dbuser="root";
-    $dbpass="";
-    $dbname="videoapi";
+	$dbhost="ec2-50-19-95-47.compute-1.amazonaws.com";
+    $dbuser="yvsdposbfkxkiq";
+    $dbpass="a8b115a6c3c9d71ce30c3131e0f46d1f5a2ecc235e1de98ceb411085307a4b07";
+    $dbname="dcofgbsdji5n1d";
     $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbConnection;
