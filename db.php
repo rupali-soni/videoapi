062dc147096490b10147cf5a550b0bde60d8a8de
Rupali Soni <rupali.soni19@gmail.com>
2017-05-01 22:15:30 +0530

    db

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ function getDB() {
     $dbuser="yvsdposbfkxkiq";
     $dbpass="a8b115a6c3c9d71ce30c3131e0f46d1f5a2ecc235e1de98ceb411085307a4b07";
     $dbname="dcofgbsdji5n1d";
-    $dbConnection = new PDO("pgsql:host=$dbhost;port=5432;dbname=$dbname", $dbuser, $dbpass);	
+    $dbConnection = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbConnection;
 }
